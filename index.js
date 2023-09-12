34f2b3e9c35b40fd38319400a40b73e6ea099f1b
Brian Mulindi <mulindijrn@gmail.com>
2023-09-13 02:40:24 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,68 @@
+number = 10;
+
+function add(){
+    return (number += 5);
+}
+add()
+
+function subtract(){
+    return (number -= 2);
+}
+subtract();
+
+function multiply(){
+    return (number *= 5);
+}
+multiply();
+
+function divide(){
+    return (number /= 5);
+}
+divide();
+
+function add(a,b){
+    return a + b;
+}
+add( 2 + 2 )
+
+function subtract(a, b){
+    return a - b;
+}
+
+subtract( 4 - 2 );
+
+function multiply(a,b){
+    return a * b;
+}
+multiply( 2 * 2 )
+
+function divide(a,b){
+    return a / b;
+}
+divive( 2 / 2 )
+
+n = 90;
+function increment(n){
+    n++
+    return n
+}
+increment();
+
+n = 100;
+function decrement(n){
+    n--
+
+    return n
+}
+decrement();
+
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+makeInt("123")
+
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
+preserveDecimal("3.14")
+
